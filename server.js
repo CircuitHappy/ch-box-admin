c6ccbf246740863bfb7cc673bec4ce068f23e7b6
vizzie <ed@vizzie.com>
2018-02-21 17:26:27 -0500

    adding sudo to the command

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ async.series([
                 if (reconfigure) {
                     console.log("\nForce reconfigure enabled - try to enable access point");
                 } else {
-                    exec("reboot", function(error, stdout, stderr) {
+                    exec("sudo reboot", function(error, stdout, stderr) {
                         console.log(stdout);
                         if (!error) console.log("rebooting system!");
                     });
