bd6da1611c85a00551be8bc2f8b064119c40a20d
vizzie <ed@vizzie.com>
2018-02-22 14:17:58 -0500

    redirect to a new page to let the user know the ML is rebooting after Wifi config.

--- app/api.js
+++ app/api.js
@@ -57,7 +57,7 @@ module.exports = function(wifi_manager, callback) {
 
         //no error checks, just return to root page
         //no process.exit, so this should just keep the webserver running
-        response.redirect("/");
+        response.redirect("reboot");
 
         // TODO: If wifi did not come up correctly, it should fail
         // currently we ignore ifup failures.
