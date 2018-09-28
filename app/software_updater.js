e2b4d9de81d7a7704cc59b3b5a5dc61fb896b9a5
Ed Guild <ed@vizzie.com>
2018-09-28 02:49:59 +0000

    added check to see if we have a new system installed, otherwise use get-latest-software from current/bin

--- app/software_updater.js
+++ app/software_updater.js
@@ -1,9 +1,11 @@
 var exec    = require("child_process").exec;
+var fs      = require("fs");
 
 /*****************************************************************************\
     Return a function which checks for software update and installs if available
 \*****************************************************************************/
 module.exports = function(cmd_options, callback) {
+    var cmd;
     // Handle case where no options are passed in
     if (typeof(cmd_options) == "function" && typeof(callback) == "undefined") {
         callback    = cmd_options;
@@ -11,7 +13,13 @@ module.exports = function(cmd_options, callback) {
     }
 
     console.log("cmd_options: " + cmd_options);
-    var cmd = 'sh /ch/system/current/scripts/ch-get-latest-software.sh';
+    // make sure we have a system newer than v1.0
+    if (fs.existsSync("/ch/system/current/scripts/ch-update-system.sh")) {
+      cmd = 'sh /ch/system/current/scripts/ch-get-latest-software.sh'
+    } else {
+      // got to deal with bug in v1.07 where get-latest-software still ran from current/bin instead of the system/scripts
+      cmd = 'sh /ch/current/bin/ch-get-latest-software.sh'
+    }
     if (cmd_options != "") {
       cmd = cmd + " " + cmd_options;
     }
